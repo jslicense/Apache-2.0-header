599f41b7a1d606fa27db38e9f6432524efe9c9b8
Kyle Mitchell <kyle@kemitchell.com>
2016-01-28 21:35:19 -0800

    Use moduleify-string

--- prepublish.js
+++
@@ -1,9 +0,0 @@
-process.stdout.write(
-  'module.exports = "' +
-    require('fs')
-      .readFileSync('/dev/stdin')
-      .toString()
-      .replace(/[\\"']/g, '\\$&')
-      .replace(/\u0000/g, '\\0')
-      .replace(/\n/g, '\\n') +
-  '"\n')
